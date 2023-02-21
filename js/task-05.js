9d36769d5dc0773c12b21d65858a879b66886d58
User0809 <agievicdaria@gmail.com>
2023-02-21 17:55:10 +0100

    update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const inputRef = document.querySelector("#name-input");
 const outputRef = document.querySelector("#name-output");
 
 inputRef.addEventListener("input", () => {
-  outputRef.textContent = inputRef.value;
+  if (inputRef.value !== "") {
+    outputRef.textContent = inputRef.value;
+  } else {
+    outputRef.textContent = "Anonymous";
+  }
 });
